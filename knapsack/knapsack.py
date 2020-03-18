6fca3ac3fcade81a9cefb9001d8cdcfab80b28cc
davidanagy <davidanagy@users.noreply.github.com>
2020-03-18 16:46:46 -0700

    completed basic knapsack algorithm (anything beyond the 'small' txt files takes a long time)

--- knapsack/knapsack.py
+++ knapsack/knapsack.py
@@ -6,7 +6,36 @@ from collections import namedtuple
 Item = namedtuple('Item', ['index', 'size', 'value'])
 
 def knapsack_solver(items, capacity):
-  pass
+   
+    def fill_item_bag(item_bag, items, capacity):
+        if len(items) == 0:
+            return item_bag
+
+        if len(item_bag) == 0:
+            total_size = 0
+            max_value = 0
+        else:
+            total_size = sum([item.size for item in item_bag])
+            max_value = sum([item.value for item in item_bag])
+        max_value_bag = item_bag
+        for item in items:
+            if (total_size + item.size) < capacity:
+                new_item_bag = item_bag.copy()
+                new_items = items.copy()
+                new_item_bag.append(item)
+                new_items.remove(item)
+                filled_bag = fill_item_bag(new_item_bag, new_items, capacity)
+                total_value = sum([item.value for item in filled_bag])
+                if total_value > max_value:
+                    max_value = total_value
+                    max_value_bag = filled_bag
+        return max_value_bag
+
+    max_value_bag = fill_item_bag([], items, capacity)
+    value = sum([item.value for item in max_value_bag])
+    index_nums = [item.index for item in max_value_bag]
+    return_dict = {'Value': value, 'Chosen': index_nums}
+    return return_dict
   
 
 if __name__ == '__main__':
